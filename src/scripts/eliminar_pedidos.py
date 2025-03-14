e484dd7c5166c809ca42e8cb1668ca94064653f4
Ian Rivera <rian27116@gmail.com>
2025-03-14 00:03:27 -0600

    Script para elminiar pedidos actualizado con apscheduler

--- src/scripts/eliminar_pedidos.py
+++ src/scripts/eliminar_pedidos.py
@@ -1,9 +1,10 @@
+from apscheduler.schedulers.background import BackgroundScheduler
 from sqlalchemy.orm import Session
 from src.database import SessionLocal
 from sqlalchemy import text
 
 def eliminar_pedidos_expirados():
-    """Elimina pedidos que tengan más de 7 días."""
+    """Función para eliminar pedidos viejos en la base de datos."""
     db: Session = SessionLocal()
     try:
         estado = 'Pendiente'
@@ -16,7 +17,31 @@ def eliminar_pedidos_expirados():
     finally:
         db.close()
 
-if __name__ == "__main__":
-    print("Ejecutando limpieza de pedidos expirados...")
-    eliminar_pedidos_expirados()
-    print("Limpieza completada.")
+# Configurar el programador
+scheduler = BackgroundScheduler()
+scheduler.add_job(eliminar_pedidos_expirados, 'cron', hour=0)  # Ejecuta todos los días a la medianoche
+scheduler.start()
+
+
+# from sqlalchemy.orm import Session
+# from src.database import SessionLocal
+# from sqlalchemy import text
+
+# def eliminar_pedidos_expirados():
+#     """Elimina pedidos que tengan más de 7 días."""
+#     db: Session = SessionLocal()
+#     try:
+#         estado = 'Pendiente'
+#         resultado = db.execute(text("DELETE FROM pedido WHERE estado = :estado"), {"estado": estado})
+#         db.commit()
+#         print(f"Pedidos eliminados: {resultado.rowcount}")
+#     except Exception as e:
+#         print(f"Error al eliminar pedidos: {e}")
+#         db.rollback()
+#     finally:
+#         db.close()
+
+# if __name__ == "__main__":
+#     print("Ejecutando limpieza de pedidos expirados...")
+#     eliminar_pedidos_expirados()
+#     print("Limpieza completada.")
